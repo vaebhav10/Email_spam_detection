65d9ce77525fbf89bed125f971e0e0628bd53db3
vaebhav10 <vaibhavkumarr59.com>
2026-07-08 15:00:48 +0530

    added lstm model

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
-import streamlit as st 
-from src.predict import predict
+import pickle
+import streamlit as st
+from src.preprocess import cleanup
 
+from tensorflow.keras.models import load_model
+from tensorflow.keras.layers import TextVectorization
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
 st.set_page_config(page_title = 'Email Spam Detection', layout= 'centered')
 st.title('Email Spam Detection')
 
 text = st.text_area('Enter your email/text :', placeholder = 'Text...')
+
+@st.cache_resource
+def load_models():
+    model = load_model(BASE_DIR/'models/Spam_detection.keras')
+    with open(BASE_DIR/'models/vocabulary.pkl','rb') as f :
+        vocab = pickle.load(f)
+        
+    vectorizer = TextVectorization(
+        max_tokens = 8000,
+        output_sequence_length = 200
+    )
+    vectorizer.set_vocabulary(vocab)
+    return model, vectorizer
+
+model, vectorizer = load_models()
+
 if st.button('predict'):
-    if text.strip():
-        st.write('Verdict : ',predict(text))
+    clean_text=  cleanup(text)
+    vec = vectorizer([clean_text])
+    y_pred = model.predict(vec)[0][0]
+    
+    if y_pred>=0.5:
+        st.write ("Spam")
     else :
-        st.write("Enter a vaild email")
+        st.write("Ham email")
+    st.write(f"Spam probability:{y_pred:.2f} ")
--- src/preprocess.py
+++ src/preprocess.py
@@ -1,25 +1,31 @@
 import re
 from bs4 import BeautifulSoup
 import nltk
-nltk.download("stopwords")
-nltk.download("wordnet")
+try:
+    nltk.data.find("corpora/stopwords")
+except LookupError:
+    nltk.download("stopwords")
+
+try:
+    nltk.data.find("corpora/wordnet")
+except LookupError:
+    nltk.download("wordnet")
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
 stp = stopwords.words("english")
+lem = WordNetLemmatizer()
 
-
-def clean_text(text):
+def cleanup(text):
     text = text.lower()
 
-    soup = BeautifulSoup(text, "html.parser")
-    text = soup.get_text(separator=" ")
+    soup = BeautifulSoup(text, 'html.parser')
+    text = soup.get_text(separator = ' ')
 
-    text = re.sub(r"[^a-zA-Z0-9\s]", "", text)
+    text = re.sub(r'[^a-zA-Z0-9\s]','', text)
 
-    lem = WordNetLemmatizer()
-    text = " ".join([lem.lemmatize(w) for w in text.split()])
-
-    text = " ".join([w for w in text.split() if w not in stp])
-
-    return text
+    text = ' '.join([lem.lemmatize (w) for w in text.split()])
+    
+    text = ' '.join([w for w  in text.split() if w not in stp])
+    
+    return text 
